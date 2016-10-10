921c611fd581304fe446116ca6dce5d271c92f92
RogelioGonzalez <rogelio.gonzalez@cetys.edu.mx>
2016-10-10 02:22:23 -0700

    Tasks card creation, CRW edition, refactoring and responsivity changes
    - Added capacity to create task cards from Backlog > New > Task (also
    added a modal for such function).
    - Added a configuration button to (non hard-coded) task cards that
    displays a modal for CRW (current remaining work) edition.
    - Code refactoring (mostly better indexing) for index.html file.
    - Some changes made to the style.css file in order to improve
    responsivity of the columns (there's still work to do on this).
    - Created a new file for scripts that access the DB.
    - Created "kanbanCardFormat.html"; a file that holds the html code to
    generate a task card (it's just a reference for development, not
    actually used in the code).

---
+++ js/dbScripts.js
@@ -0,0 +1,43 @@
+function addTaskToDB (taskId, desc, duration, deadline, owner, column, sprint) {
+	//TAREA: hacer este metodo (querie)
+	
+	// NOTAS:
+	// El crw es igual a duration al ser creado
+	// El owner que se envia por aqui es un nombre, llamar a otro metodo que recupere su ID, pues eso espera la tabla  de la BD
+}
+
+function getCRW(id) { //debe recibir el id de la tarea y regresar el Current Remaining Time de la misma
+	var crw = 4; // el numero es placeholder para probar
+	
+	// TAREA: Asignar a crw el valor recuperado de la BD, enviando el id de la tarea
+	
+	//sessionStorage.selectedCRW = crw;
+	//console.log(sessionStorage.selectedCRW);
+	return crw;
+}
+
+function setCRT() {
+	if(name_signup_valid!=false && lastname_signup_valid!=false && email_signup_valid !=false && password_signup_valid!=false && password2_signup_valid!=false){
+		
+		var formData = {
+            "id": $(/* SE DEBE RECUPERAR EL VALOR ID DE LA TARJETA A LA QUE SE LE DIO CLICK*/).val(),
+            "crt": $('#crt_card_input').val()
+        }; // ESTO DEBE CORREGIRSE YA QUE ESTE LA TABLA PARA TARJETAS DE TAREAS
+       
+       $.ajax({
+            url: "http://trelloagilprueba.esy.es/agiltrello/api/setCRT", // ESTE URL DEBE CORREGIRSE (NO EXISTE)
+            type: 'POST',
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            encode: true
+        }).done(function (data) {
+        	console.log(data);
+			window.location.href="index.html";
+        }).fail(function (data) {
+            console.log(data);
+        });
+      } else {
+      	 $("#error_required").show();
+      console.log("Campos requeridos");
+    }
+}
