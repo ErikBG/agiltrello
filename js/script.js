066dc3daaa39c4dfa1e6aa4c21e0d41a87607480
Ricardo Velazquez <ricardo.velazquez.serna@hotmail.com>
2016-10-10 22:47:45 -0700

    se agrego un metodo para que consiga el id del sprint cuando se cambie

--- js/script.js
+++ js/script.js
@@ -4,12 +4,29 @@ $.get('http://localhost/agiltrello/api/getsprint', function (data) {
   var html_code = '<option value="id">sprint</option>';
   $.each(data, function (i, sprint) {
     var current_html = html_code;
-    current_html = current_html.replace("id", sprint['Id']);
+    current_html = current_html.replace("id", sprint['id']);
     current_html = current_html.replace("sprint", sprint['name']);
     $('#sprint_select').append(current_html);
+    var default_val=$('#sprint_select').val();
+   
   });
 });
-
+//get sprint with stories
+$(document).ready(function () {
+    $("#sprint_select").change(function () {
+      var id_sprint = $("#sprint_select").val();
+      // $.get('http://localhost/School/api/getsubjects', {idc,ns}, function (data) {
+      //   var html_code = '<option value="id">subject</option>';
+      //   $.each(data, function (i, subject) {
+      //     var current_html = html_code;
+      //     current_html = current_html.replace("id", subject['id']);
+      //     current_html = current_html.replace("subject", subject['name']);
+      //     $('#subject').append(current_html);
+        // });
+        
+      // });
+    }
+  });
 
 //get user_story
 $.get('http://localhost/agiltrello/api/getcard', function (data) {
